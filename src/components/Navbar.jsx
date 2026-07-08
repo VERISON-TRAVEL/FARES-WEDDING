cdffaa13ed716d75d302792aa6358c742213b643
MAIN-GIT <verison.2023@gmail.com>
2026-07-08 15:18:06 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => (
   <nav className="navbar" role="navigation" aria-label="شعار الحفل">
     <div className="nav-container" style={{ justifyContent: 'center' }}>
-      <div className="nav-brand" aria-label="سناء وفارس">سناء &amp; فارس</div>
+      <div className="nav-brand" aria-label="سناء وفارس">فارس &amp; سناء</div>
     </div>
   </nav>
 )
